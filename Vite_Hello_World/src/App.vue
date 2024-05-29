9dd0b56f2641766981e23cf1e2fc3fea886c32f6
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-29 16:29:16 +0200

    added header

--- Vite_Hello_World/src/App.vue
+++ Vite_Hello_World/src/App.vue
@@ -1,17 +1,26 @@
 <script>
 import Paragrafo from "./components/Paragraf.vue";
 import Titolo from "./components/Apptitle.vue";
+import Header from "./components/Header.vue";
 export default {
   components: {
     Paragrafo,
     Titolo,
+    Header,
   },
 };
 </script>
 
 <template>
-  <Titolo></Titolo>
-  <Paragrafo></Paragrafo>
+  <Header></Header>
+  <main>
+    <Titolo></Titolo>
+    <Paragrafo></Paragrafo>
+  </main>
 </template>
 
-<style></style>
+<style scoped>
+main {
+  margin-top: 100px;
+}
+</style>
---
+++ Vite_Hello_World/src/components/Header.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      title: "questo e il mio primo progetto con vite",
+    };
+  },
+};
+</script>
+
+<template>
+  <header></header>
+</template>
+
+<style scoped>
+header {
+  height: 50px;
+  background-color: aqua;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+}
+</style>
