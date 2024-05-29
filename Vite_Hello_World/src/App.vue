29ad8d2b42eca14885398e6b0f627a030b42f521
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-29 16:03:09 +0200

    first commit

---
+++ Vite_Hello_World/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <h1>Questo e il titolo che ho inserito io</h1>
+</template>
+
+<style></style>
---
+++ Vite_Hello_World/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "title",
+};
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <style scoped>
+    .read-the-docs {
+      color: #888;
+    }
+  </style>
+</template>
